f19c3a6ed12769d3122a6ab6fd7cf63ad353d954
andresrgallo <andresgallo@hotmail.com>
2019-02-01 08:06:27 +1100

    Update Address details

--- src/components/resumeData.jsx
+++ src/components/resumeData.jsx
@@ -2,7 +2,7 @@ const resumeData = {
 	imagebaseurl: 'http://localhost:3000/',
 	name: 'Andres Rodriguez Gallo',
 	role: 'Junior Full Stack Web Developer',
-	address: '68 Rathowen Pde',
+	address: '306/47 Chippen St',
 	suburbstate: 'Chippendale NSW 2008',
 	mobile: '0434865906',
 	email: 'andresgallo@hotmail.com'
